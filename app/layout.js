f4b81bb0c1492577c1368b4f47a15832729bdd85
webgawd-apex <omdjr24life@gmail.com>
2026-04-06 18:48:10 +0100

    fix: Final Vercel pnpm stabilization and Coinflip UI refinements (centering + scrolling)

--- app/layout.js
+++ app/layout.js
@@ -22,9 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-zinc-950 text-white">
+      <body className="flex flex-col bg-zinc-950 text-white">
         <WalletContextProvider>
           {children}
         </WalletContextProvider>
