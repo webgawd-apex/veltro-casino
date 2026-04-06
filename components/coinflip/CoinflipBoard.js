dd9fa5a61dcb6721d112ff9ec89a11fc47336664
webgawd-apex <omdjr24life@gmail.com>
2026-04-06 21:17:07 +0100

    UI: Final mobile stabilization, responsive 3D containers, and fixed wallet button centering

--- components/coinflip/CoinflipBoard.js
+++ components/coinflip/CoinflipBoard.js
@@ -4,7 +4,7 @@ import ThreeCoin from './ThreeCoin';
 
 export default function CoinflipBoard({ selectedSide, isFlipping, result, winStatus }) {
   return (
-    <div className="flex flex-col items-center justify-center pt-2 pb-0 px-4 pl-[15px] lg:pl-[15px] relative z-10 w-full mb-0">
+    <div className="flex flex-col items-center justify-center pt-2 pb-0 px-4 pl-[35px] lg:pl-[35px] relative z-10 w-full mb-0">
       <div className="flex flex-col items-center justify-center space-y-1 ml-[35px]">
         <ThreeCoin selectedSide={selectedSide} isFlipping={isFlipping} result={result} />
         
